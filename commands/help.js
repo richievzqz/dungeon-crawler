3983897ab3a0ff4359cfcd7df8f0a941e33798be
richie <richie.vzqz@gmail.com>
2020-06-14 23:39:18 -0500

    Test Commands added

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'help',
+    description: 'Help page',
+    execute(message, args) {
+        const embed = new Discord.MessageEmbed(); 
+        embed.setTitle('Help is here!');
+        embed.setDescription('Here`s a list of available commands.');
+        for (const command of message.client.commands.keys())
+        {
+            embed.addField(`${message.client.commands.get(command).name}`,
+             `${message.client.commands.get(command).description}`);
+        }
+        message.channel.send(embed);
+    }
+};
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+const fs = require('fs'); // This is Node's native file system module.
 const Discord = require('discord.js');
-const config = require('./config.json');
-const client = new Discord.Client();
+const { prefix, token } = require ('./config.json');
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.on('message', message => {
-	if (message.content === '!ping') {
-		message.channel.send('Pong.');
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if(!client.commands.has(commandName)) return;
+
+	const command = client.commands.get(commandName);
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('There was an error trying to execute that command');
 	}
+
 });
 
-client.login(config.token);
+client.login(token);
